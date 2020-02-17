def442551c0a0d83e3cbfcd030693663596a3a25
Pig <pig.priv@gmail.com>
2020-02-17 15:43:03 +0100

    davinci: fod: Remove duplicated setTouchMode onShowFODView
    Change-Id: I8936f97201bff64b7d526b4f974bf929d5e7c3a6

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -105,7 +105,6 @@ Return<void> FingerprintInscreen::onRelease() {
 
 Return<void> FingerprintInscreen::onShowFODView() {
     set(DIM_LAYER_PATH, 1);
-    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
     this->mFodCircleVisible = true;
     return Void();
 }
