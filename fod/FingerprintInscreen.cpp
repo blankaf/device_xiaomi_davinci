44474a905390e9c5580164c607266a4d7cf01f29
raysenlau <raysen@live.cn>
2020-02-20 21:59:02 +0800

    davinci: Fod Improvements
     * Optimize code logic

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -98,7 +98,7 @@ Return<void> FingerprintInscreen::onPress() {
 
 Return<void> FingerprintInscreen::onRelease() {
     set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
-    TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
+    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 0);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
     return Void();
 }
@@ -112,7 +112,7 @@ Return<void> FingerprintInscreen::onShowFODView() {
 Return<void> FingerprintInscreen::onHideFODView() {
     set(DIM_LAYER_PATH, 0);
     set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
-    TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
+    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 0);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
     this->mFodCircleVisible = false;
     return Void();
