448c99bfe1257216d122e13e260a87e8e559d3f1
blankaf <realblankaf@gmail.com>
2020-04-23 11:05:26 +0000

    davinci: Remove snap from device.mk

--- device.mk
+++ device.mk
@@ -54,10 +54,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
     $(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml \
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
